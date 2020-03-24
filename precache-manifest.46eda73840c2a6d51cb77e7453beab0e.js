1542df7b6d091e17763836612434e4e0ceaa3453
unknown <arturbohanov1908@gmail.com>
2020-03-24 12:50:35 +0200

    Updates

---
+++ precache-manifest.46eda73840c2a6d51cb77e7453beab0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a334dc971fad420fc48ca13a44bcdb7",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "f93d9422c28336306a99",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.6d048306.chunk.css"
+  },
+  {
+    "revision": "52ee0685af9baed16885",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.49f495b0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.49f495b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f93d9422c28336306a99",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.8a00a862.chunk.js"
+  },
+  {
+    "revision": "4992c48e99877600891f",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.ad90512f.js"
+  }
+]);
